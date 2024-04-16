4f87efa419c2f4d9402a587fbaa0b1c0fc935bb8
Rajiv aryal <rajivary1@gmail.com>
2024-04-16 21:33:11 +0545

    LOgin ssytem

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ define('DB_USERNAME' , 'root');
 define('DB_PASSWORD' , '');
 define('DB_NAME' , 'login-system');
 
-$link mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
 if ($link==false)
 {
